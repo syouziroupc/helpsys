c07b421c8368b5ed9f6ed0f39bc99f643ffb30f8
syouziroupc <syouziroupc@gmail.com>
2026-09-09 11:32:58 +0900

    Education v2: validate curriculum before UI launch

--- src/HelpSys.Education/App.xaml.cs
+++ src/HelpSys.Education/App.xaml.cs
@@ -4,4 +4,18 @@ namespace HelpSys.Education;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        Curriculum.Validate();
+
+        if (e.Args.Any(x => string.Equals(x, "--self-test", StringComparison.OrdinalIgnoreCase)))
+        {
+            Shutdown(0);
+            return;
+        }
+
+        base.OnStartup(e);
+        MainWindow = new MainWindow();
+        MainWindow.Show();
+    }
 }
