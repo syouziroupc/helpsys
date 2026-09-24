a767e4e6bc1eb8d919aa835bb1e2ab67f590f24b
syouziroupc <syouziroupc@gmail.com>
2026-09-24 21:08:51 +0900

    Force production redeploy for Outlaw 3.2.0 worker

--- worker/stable-router.js
+++ worker/stable-router.js
@@ -3,7 +3,7 @@ import education from './education-gemini.js';
 import outlaw from './outlaw-guide.js';
 import transcribe from './transcribe.js';
 
-const SITE_REVISION = 'a3.0001-outlaw-3.1.9';
+const SITE_REVISION = 'a3.0001-outlaw-3.2.0';
 
 export default {
   async fetch(request, env, ctx) {
