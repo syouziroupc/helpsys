5acad51f55e3f8c0dfabe4d34515ec9a8a119adf
syouziroupc <syouziroupc@gmail.com>
2026-09-24 23:26:02 +0900

    Site health: identify Outlaw 3.2.3 publication

--- worker/stable-router.js
+++ worker/stable-router.js
@@ -3,7 +3,7 @@ import education from './education-gemini.js';
 import outlaw from './outlaw-guide.js';
 import transcribe from './transcribe.js';
 
-const SITE_REVISION = 'a3.0001-outlaw-3.2.2-4905f758';
+const SITE_REVISION = 'a3.0001-outlaw-3.2.3-ad3ee9b6';
 
 export default {
   async fetch(request, env, ctx) {
