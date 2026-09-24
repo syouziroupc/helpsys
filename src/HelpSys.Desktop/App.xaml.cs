6b37a75987219ad8e68d72efe9449a17977d8512
syouziroupc <syouziroupc@gmail.com>
2026-09-24 23:23:44 +0900

    Outlaw 3.2.3: capture abnormal process exits

--- src/HelpSys.Desktop/App.xaml.cs
+++ src/HelpSys.Desktop/App.xaml.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
 using HelpSys.Services;
 
 namespace HelpSys;
@@ -26,14 +27,20 @@ public partial class App : Application
             return;
         }
 
+        LocalLogService.Initialize();
+        DispatcherUnhandledException += OnDispatcherUnhandledException;
+        SessionEnding += OnSessionEnding;
+        AppDomain.CurrentDomain.UnhandledException += OnAppDomainUnhandledException;
+        TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
+
         _singleInstanceMutex = new Mutex(initiallyOwned: true, name: @"Local\HelpSys.Desktop.SingleInstance", createdNew: out var createdNew);
         if (!createdNew)
         {
+            LocalLogService.MarkExitReason("single_instance_rejected", overwrite: true);
             Shutdown();
             return;
         }
 
-        LocalLogService.Initialize();
         base.OnStartup(e);
         MainWindow = new MainWindow();
         MainWindow.Show();
@@ -49,12 +56,43 @@ public partial class App : Application
 
     protected override void OnExit(ExitEventArgs e)
     {
+        LocalLogService.MarkExitReasonIfUnset("application_exit");
+        LocalLogService.Write("application_exit", $"code={e.ApplicationExitCode};reason={LocalLogService.ExitReason}");
+
         try { _smokeListeningOverlay?.Close(); } catch { }
         _smokeListeningOverlay = null;
         try { _singleInstanceMutex?.ReleaseMutex(); } catch (ApplicationException) { }
         _singleInstanceMutex?.Dispose();
         try { HelpSys.Services.UiAutomationScanner.ShutdownSharedObserver(); } catch { }
+        DispatcherUnhandledException -= OnDispatcherUnhandledException;
+        SessionEnding -= OnSessionEnding;
+        AppDomain.CurrentDomain.UnhandledException -= OnAppDomainUnhandledException;
+        TaskScheduler.UnobservedTaskException -= OnUnobservedTaskException;
         LocalLogService.Shutdown();
         base.OnExit(e);
+    }
+
+    private static void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        LocalLogService.MarkExitReason("dispatcher_unhandled_exception", e.Exception.GetType().FullName, overwrite: true);
+        LocalLogService.WriteException("dispatcher_unhandled_exception", e.Exception);
+    }
+
+    private static void OnAppDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        LocalLogService.MarkExitReason("appdomain_unhandled_exception", overwrite: true);
+        if (e.ExceptionObject is Exception exception)
+            LocalLogService.WriteException("appdomain_unhandled_exception", exception);
+        else
+            LocalLogService.Write("appdomain_unhandled_exception", $"terminating={e.IsTerminating};value={e.ExceptionObject}");
+    }
+
+    private static void OnUnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+        => LocalLogService.WriteException("unobserved_task_exception", e.Exception);
+
+    private static void OnSessionEnding(object sender, SessionEndingCancelEventArgs e)
+    {
+        LocalLogService.MarkExitReason("windows_session_ending", e.ReasonSessionEnding.ToString(), overwrite: true);
+        LocalLogService.Write("windows_session_ending", e.ReasonSessionEnding.ToString());
     }
 }
