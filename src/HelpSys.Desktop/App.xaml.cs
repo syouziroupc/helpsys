627240ca67fac591112094c6b9fad63776876bd1
syouziroupc <syouziroupc@gmail.com>
2026-09-24 23:18:02 +0900

    Record HelpSys process exit and unhandled exception reasons

--- src/HelpSys.Desktop/App.xaml.cs
+++ src/HelpSys.Desktop/App.xaml.cs
@@ -1,5 +1,7 @@
 using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
+using HelpSys.Services;
 
 namespace HelpSys;
 
@@ -10,9 +12,14 @@ public partial class App : Application
 
     protected override void OnStartup(StartupEventArgs e)
     {
+        ProcessDiagnostics.Initialize();
+        DispatcherUnhandledException += OnDispatcherUnhandledException;
+        SessionEnding += OnSessionEnding;
+
         _singleInstanceMutex = new Mutex(initiallyOwned: true, name: @"Local\HelpSys.Desktop.SingleInstance", createdNew: out var createdNew);
         if (!createdNew)
         {
+            ProcessDiagnostics.MarkExitReason("single_instance_rejected", overwrite: true);
             Shutdown();
             return;
         }
@@ -32,10 +39,31 @@ public partial class App : Application
 
     protected override void OnExit(ExitEventArgs e)
     {
+        ProcessDiagnostics.MarkExitReasonIfUnset("application_exit");
+        ProcessDiagnostics.Log("application_exit", new
+        {
+            e.ApplicationExitCode,
+            exitReason = ProcessDiagnostics.ExitReason
+        });
+
         try { _smokeListeningOverlay?.Close(); } catch { }
         _smokeListeningOverlay = null;
         try { _singleInstanceMutex?.ReleaseMutex(); } catch (ApplicationException) { }
         _singleInstanceMutex?.Dispose();
+        DispatcherUnhandledException -= OnDispatcherUnhandledException;
+        SessionEnding -= OnSessionEnding;
         base.OnExit(e);
+    }
+
+    private static void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        ProcessDiagnostics.MarkExitReason("dispatcher_unhandled_exception", e.Exception.GetType().FullName, overwrite: true);
+        ProcessDiagnostics.LogException("dispatcher_unhandled_exception", e.Exception);
+    }
+
+    private static void OnSessionEnding(object sender, SessionEndingCancelEventArgs e)
+    {
+        ProcessDiagnostics.MarkExitReason("windows_session_ending", e.ReasonSessionEnding.ToString(), overwrite: true);
+        ProcessDiagnostics.Log("windows_session_ending", new { reason = e.ReasonSessionEnding.ToString() });
     }
 }
